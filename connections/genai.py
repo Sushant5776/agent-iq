5f647337ab538d4c473d448945399b7f4909a918
Sushant Garudkar <garudkar.sa@gmail.com>
2026-08-06 17:34:45 +0530

    safeguard client in genai

--- connections/genai.py
+++ connections/genai.py
@@ -9,14 +9,14 @@ gemini_api_key = os.environ.get("GEMINI_API_KEY", None)
 
 
 class GenAI:
-    client = None
+    __client = None
 
     @staticmethod
     def get_client():
         if not gemini_api_key:
             raise ValueError("GenAI:get_client - GEMINI_API_KEY not set!")
 
-        if not GenAI.client:
-            GenAI.client = genai.Client(api_key=gemini_api_key, vertexai=False)
+        if not GenAI.__client:
+            GenAI.__client = genai.Client(api_key=gemini_api_key, vertexai=False)
 
-        return GenAI.client
+        return GenAI.__client
