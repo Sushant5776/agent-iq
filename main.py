a4e515812c3b8bdcff92c580c454c0af35becd76
Sushant Garudkar <garudkar.sa@gmail.com>
2026-08-06 17:46:58 +0530

    check response from model and llm connectivity

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from google.genai.types import Part
+
+from connections.genai import GenAI
+
+load_dotenv()
+genai_client = GenAI.get_client()
+model = os.environ.get("LANGUAGE_MODEL", "None")
+
+
 def main():
     print("Hello from agent-iq!")
+    response = genai_client.models.generate_content(
+        model=model, contents=[Part(text="Hi, How are you?")]
+    )
+    print(response.text)
 
 
 if __name__ == "__main__":
